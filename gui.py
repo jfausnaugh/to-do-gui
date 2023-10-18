931e6dbf3316523afb93beeee98a6a53f23b8f59
Juliette Fausnaugh <j.d.land.16@gmail.com>
2023-10-18 12:53:37 -0400

    Implemented the Add button

--- gui.py
+++ gui.py
@@ -4,9 +4,23 @@ import functions
 import PySimpleGUI
 
 label = PySimpleGUI.Text("Type in a to-do")
-input_box = PySimpleGUI.InputText(tooltip="Enter todo")
+input_box = PySimpleGUI.InputText(tooltip="Enter todo", key="todo")
 add_button = PySimpleGUI.Button("Add")
-window = PySimpleGUI.Window('My To-Do App', layout=[[label], [input_box, add_button]])  # this is the title of the window application
+window = PySimpleGUI.Window('My To-Do App',
+                            layout=[[label], [input_box, add_button]],
+                            font=('Helvetica', 20))  # this is the title of the window application
 #  each square bracket around the items above represent a new row. If all in one bracket, that's one row
-window.read()
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case PySimpleGUI.WIN_CLOSED:
+            break
+
 window.close()
